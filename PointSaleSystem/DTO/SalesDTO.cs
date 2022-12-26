f1ab312d0b6665d66addeb1d476bccce3ebdcbde
Syeda Farogh Zahra <102902583+FaroghZahra@users.noreply.github.com>
2022-12-26 20:59:55 +0500

    DTO added

---
+++ PointSaleSystem/DTO/SalesDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class SalesDTO
+    {
+        public int ID { get; set; }
+        public int CustomerID { get; set; }
+        public DateTime Date { get; set; }
+        public string status { get; set; }
+    }
+}
