4ea5327af325ec9ffd1d2f971d53bf361f5df368
Syeda Farogh Zahra <102902583+FaroghZahra@users.noreply.github.com>
2022-12-26 20:45:38 +0500

    Add files via upload

---
+++ PointSaleSystem/BCSF19M548_Assignment01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using PL;
+
+namespace BCSF19M548_Assignment01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Running Program interface
+            POS_PL pos = new POS_PL();
+            pos.MainMenu();
+        }
+    }
+}
